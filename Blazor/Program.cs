7bf115ad0c6696b52868ade8ff9cda8edecd5586
Aidan <1061551@hr.nl>
2024-06-28 02:01:36 +0200

    Working

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -1,3 +1,4 @@
+/*
 using Blazor.Components;
 using Blazor.Components.Account;
 using Blazor.Data;
@@ -110,6 +111,132 @@ using (var scope = app.Services.CreateScope())
         var result = await userManager.CreateAsync(user, password);
         await userManager.AddToRoleAsync(user, "Admin");
     }
+}
+
+app.Run();
+*/
+//10 default rooms now
+
+using Blazor.Components;
+using Blazor.Components.Account;
+using Blazor.Data;
+using Blazor.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+builder.Services.AddMudServices();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    // Microsoft Authentication (not complete)
+    // .AddMicrosoftAccount(microsoftOptions =>
+    // {
+    //     microsoftOptions.ClientId = builder.Configuration["Authentication:Microsoft:ClientId"];
+    //     microsoftOptions.ClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"];
+    // })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("Database") ??
+                       throw new InvalidOperationException("Connection string 'Database' not found.");
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddSignInManager()
+    .AddRoles<IdentityRole>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
+    .AddRoleStore<RoleStore<IdentityRole, AppDbContext>>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<User>, IdentityNoOpEmailSender>();
+
+builder.Services.AddAuthorizationBuilder()
+    .AddFallbackPolicy("RequireAuthenticatedUser", policyBuilder =>
+    {
+        policyBuilder.RequireAuthenticatedUser().Build();
+    });
+
+builder.Services.AddTransient<RoomService>();
+builder.Services.AddTransient<ReservationService>();
+builder.Services.AddTransient<WorkspaceService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+// User & Role seeding
+using (var scope = app.Services.CreateScope())
+{
+    const string email = "admin@admin.com";
+    const string password = "Admin1!";
+    string[] roles = ["Admin", "Manager", "User"];
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            var roleRole = new IdentityRole(role);
+            await roleManager.CreateAsync(roleRole);
+        }
+    }
+    if (await userManager.FindByNameAsync(email) == null)
+    {
+        var user = new User
+        {
+            UserName = email,
+            Email = email,
+            EmailConfirmed = true
+        };
+        var result = await userManager.CreateAsync(user, password);
+        await userManager.AddToRoleAsync(user, "Admin");
+    }
+
+    // Seed default rooms
+    /*var roomService = scope.ServiceProvider.GetRequiredService<RoomService>();
+    await roomService.SeedDefaultRoomsAsync();*/
+    
+    
 }
 
 app.Run();
