5cc221ac41cf71b56a58802064680bade4f71827
Aidan <1061551@hr.nl>
2024-07-03 17:15:42 +0200

    Added missing files

---
+++ Blazor/Migrations/20240701200514_AddFloors.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFloors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Floor",
+                table: "Reservations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Floor",
+                table: "Reservations");
+        }
+    }
+}
